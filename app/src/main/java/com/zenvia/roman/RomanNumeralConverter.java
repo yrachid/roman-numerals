0443e5e96e38120634a9b4d443b29c925b968e19
yrachid <anuar.yasser@gmail.com>
2019-12-04 11:42:37 -0300

    Converte valores unitarios (menores que 10)
    Implementa a conversao de valores unitarios (menores que 10) utilizando
    uma logica bastante simples e repetitiva. A ideia eh poder utilizar esta
    conversao simplificada para converter numeros mais complexos. Depois que
    a conversao de valores mais complexos estiver funcionando, pode ser
    interessante refatorar esse codigo.

--- app/src/main/java/com/zenvia/roman/RomanNumeralConverter.java
+++ app/src/main/java/com/zenvia/roman/RomanNumeralConverter.java
@@ -6,7 +6,6 @@ import java.util.stream.Stream;
 public class RomanNumeralConverter {
 
     private enum RomanNumeral {
-
         I(1),
         V(5),
         X(10),
@@ -26,16 +25,57 @@ public class RomanNumeralConverter {
                     .filter(roman -> roman.arabicValue == arabicValue)
                     .findFirst();
         }
+
+        public static boolean hasDirectEquivalent(int arabicValue) {
+            return Stream.of(values())
+                    .anyMatch(roman -> roman.arabicValue == arabicValue);
+        }
     }
 
     public static String convert(int arabicValue) {
 
-        if (arabicValue > 3000) {
+        if (arabicValue > 3000 || arabicValue < 1) {
             return "ERROR";
         }
 
-        return RomanNumeral.fromArabicValue(arabicValue)
-                .map(Enum::name)
-                .orElse(Integer.toString(arabicValue));
+        if (RomanNumeral.hasDirectEquivalent(arabicValue)) {
+            return RomanNumeral.fromArabicValue(arabicValue)
+                    .map(Enum::name)
+                    .orElse(Integer.toString(arabicValue));
+        }
+
+        if (arabicValue < 10) {
+            return convertUnit(arabicValue);
+        }
+
+        return Integer.toString(arabicValue);
+    }
+
+    private static String convertUnit(int value) {
+        if (value == 2) {
+            return "II";
+        }
+
+        if (value == 3) {
+            return "III";
+        }
+
+        if (value == 4) {
+            return "IV";
+        }
+
+        if (value == 6) {
+            return "VI";
+        }
+
+        if (value == 7) {
+            return "VII";
+        }
+
+        if (value == 8) {
+            return "VIII";
+        }
+
+        return "IX";
     }
 }
--- app/src/test/java/RomanNumeralConverterTest.java
+++ app/src/test/java/RomanNumeralConverterTest.java
@@ -1,26 +1,37 @@
-import com.zenvia.roman.RomanNumeralConverter;
 import org.junit.Test;
 
+import static com.zenvia.roman.RomanNumeralConverter.convert;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class RomanNumeralConverterTest {
 
     @Test
-    public void must_not_convert_values_greater_than_3000() {
-        assertThat(RomanNumeralConverter.convert(3001), equalTo("ERROR"));
-        assertThat(RomanNumeralConverter.convert(3000), equalTo("3000"));
+    public void must_not_convert_values_greater_than_3000_or_smaller_than_1() {
+        assertThat(convert(3001), equalTo("ERROR"));
+        assertThat(convert(0), equalTo("ERROR"));
     }
 
     @Test
-    public void converts_single_numerals() {
-        assertThat(RomanNumeralConverter.convert(1), equalTo("I"));
-        assertThat(RomanNumeralConverter.convert(5), equalTo("V"));
-        assertThat(RomanNumeralConverter.convert(10), equalTo("X"));
-        assertThat(RomanNumeralConverter.convert(50), equalTo("L"));
-        assertThat(RomanNumeralConverter.convert(100), equalTo("C"));
-        assertThat(RomanNumeralConverter.convert(500), equalTo("D"));
-        assertThat(RomanNumeralConverter.convert(1000), equalTo("M"));
+    public void converts_values_with_direct_equivalent_numerals() {
+        assertThat(convert(1), equalTo("I"));
+        assertThat(convert(5), equalTo("V"));
+        assertThat(convert(10), equalTo("X"));
+        assertThat(convert(50), equalTo("L"));
+        assertThat(convert(100), equalTo("C"));
+        assertThat(convert(500), equalTo("D"));
+        assertThat(convert(1000), equalTo("M"));
     }
 
+    @Test
+    public void converts_units() {
+        assertThat(convert(2), equalTo("II"));
+        assertThat(convert(3), equalTo("III"));
+        assertThat(convert(4), equalTo("IV"));
+        assertThat(convert(5), equalTo("V"));
+        assertThat(convert(6), equalTo("VI"));
+        assertThat(convert(7), equalTo("VII"));
+        assertThat(convert(8), equalTo("VIII"));
+        assertThat(convert(9), equalTo("IX"));
+    }
 }
