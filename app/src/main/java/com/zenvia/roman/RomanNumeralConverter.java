e709b44576b82a72628f917cbd8b897c22e208bc
yrachid <anuar.yasser@gmail.com>
2019-12-03 23:27:42 -0300

    Suporte a algarismos individuais
    Nesta iteracao, o conversor suporta apenas algarismos individuais. Isto
    ajudara a memorizar quais sao os algarismos que representam cada valor.

--- app/src/main/java/com/zenvia/roman/RomanNumeralConverter.java
+++ app/src/main/java/com/zenvia/roman/RomanNumeralConverter.java
@@ -7,6 +7,34 @@ public class RomanNumeralConverter {
             return "ERROR";
         }
 
+        if (arabicValue == 1) {
+            return "I";
+        }
+
+        if (arabicValue == 5) {
+            return "V";
+        }
+
+        if (arabicValue == 10) {
+            return "X";
+        }
+
+        if (arabicValue == 50) {
+            return "L";
+        }
+
+        if (arabicValue == 100) {
+            return "C";
+        }
+
+        if (arabicValue == 500) {
+            return "D";
+        }
+
+        if (arabicValue == 1000) {
+            return "M";
+        }
+
         return Integer.toString(arabicValue);
     }
 }
--- app/src/test/java/RomanNumeralConverterTest.java
+++ app/src/test/java/RomanNumeralConverterTest.java
@@ -11,4 +11,39 @@ public class RomanNumeralConverterTest {
         assertThat(RomanNumeralConverter.convert(3001), equalTo("ERROR"));
         assertThat(RomanNumeralConverter.convert(3000), equalTo("3000"));
     }
+
+    @Test
+    public void converts_1_to_I() {
+        assertThat(RomanNumeralConverter.convert(1), equalTo("I"));
+    }
+
+    @Test
+    public void converts_5_to_V() {
+        assertThat(RomanNumeralConverter.convert(5), equalTo("V"));
+    }
+
+    @Test
+    public void converts_10_to_X() {
+       assertThat(RomanNumeralConverter.convert(10), equalTo("X"));
+    }
+
+    @Test
+    public void converts_50_to_L() {
+        assertThat(RomanNumeralConverter.convert(50), equalTo("L"));
+    }
+
+    @Test
+    public void converts_100_to_C() {
+        assertThat(RomanNumeralConverter.convert(100), equalTo("C"));
+    }
+
+    @Test
+    public void converts_500_to_D() {
+        assertThat(RomanNumeralConverter.convert(500), equalTo("D"));
+    }
+
+    @Test
+    public void converts_1000_to_M() {
+        assertThat(RomanNumeralConverter.convert(1000), equalTo("M"));
+    }
 }
